20839c106c08e4c46cd2932ef808138f66c8c1a0
Doniyor <doniyorshavkati@gmail.com>
2023-07-29 10:22:05 +0500

    first test

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,69 @@
-def reverse(string):
-    return string[::-1]
+def _right(coll, path, value):
+    length = len(path)
+    last_index = length - 1
+    index = 0
+    nested = coll
+
+    while index < length:
+        key = path[index]
+        new_value = value
+        if index != last_index:
+            obj_value = nested[key] if key in nested else None
+            new_value = obj_value if isinstance(obj_value, dict) else {}
+        nested[key] = new_value
+        nested = nested[key]
+        index += 1
+    return coll
+
+
+def _wrong1(coll, path, value):
+    length = len(path)
+    last_index = length - 1
+    index = 0
+    nested = coll
+
+    while index < length:
+        key = path[index]
+        new_value = value
+        if index != last_index:
+            obj_value = nested[key] if key in nested else None
+            new_value = obj_value if isinstance(obj_value, dict) else {}
+        nested[key] = new_value
+        nested = nested[key]
+        index += 1
+    coll['key'] = 'value'
+    return coll
+
+
+def _wrong2(coll, path, value):
+    length = len(path)
+    last_index = length - 1
+    index = 0
+    nested_coll = coll
+
+    while index < length:
+        key = path[index]
+        new_value = value
+        if index != last_index:
+            new_value = {}
+        nested_coll[key] = new_value
+        nested_coll = nested_coll[key]
+        index += 1
+    return coll
+
+
+def _wrong3(coll, path, value):
+    if not path:
+        return coll
+    key = path[0]
+    coll[key] = value
+    return coll
+
+
+def _wrong4(coll, path, value):
+    if len(path) <= 1:
+        return coll
+    return _right(coll, path, value)
+coll = {'a': {'b': {'c': 3}}}
+
+print(_right(coll, ['a', 'b', 'c'], 4))
